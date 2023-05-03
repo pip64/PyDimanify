453b31abf4ecd5dd722655c72cf95abbfa7a0954
pip64 <76626915+pip64@users.noreply.github.com>
2023-05-04 00:37:32 +0400

    v1.4.1

--- core/disetify.py
+++ core/disetify.py
@@ -1,19 +1,7 @@
 import disnake
 from disnake.ext import commands
 import re
-from dimanify import *
-from core.markov import *
-from core.tryexcept import *
-from core.conditions import *
-from core.files import *
-from core.variables import *
-from core.texts import *
-from core.randoms import *
-from core.funcs import *
-from core.other import *
-from core.requests import *
-from core.cycles import *
-#from core.disetify import *
+
 
 class Бот:
   def __init__(self, префикс, токен, интенты):
--- core/requests.py
+++ core/requests.py
@@ -1,17 +1,4 @@
 import requests
-from dimanify import *
-from core.markov import *
-from core.tryexcept import *
-from core.conditions import *
-from core.files import *
-from core.variables import *
-from core.texts import *
-from core.randoms import *
-from core.funcs import *
-from core.other import *
-from core.requests import *
-from core.cycles import *
-#from core.disetify import *
 
 class запрос:
     def __init__(self, url):
