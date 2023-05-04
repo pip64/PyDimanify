6ecf6e5b1e7e1abc47fcc8d8fdc760d7a8d9c407
pip64 <76626915+pip64@users.noreply.github.com>
2023-05-04 23:16:11 +0400

    Update other.py

--- core/other.py
+++ core/other.py
@@ -4,8 +4,7 @@ from dimanify import *
 def диапазон(число):
     if not isinstance(число, int):
         raise TypeError("Число должно быть целым числом")
-    return range(число)
-
+    return [i for i in range(число)]
 
 def считать(что):
     if not isinstance(что, (list, str)):
